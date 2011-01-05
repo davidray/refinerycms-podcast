210c97bb236f4eb31c18286009496d92c17b2fca
davidray <dave@developertown.com>
2011-01-05 17:25:44 -0600

    Cleaning up the player

--- lib/jw_player_helper.rb
+++ lib/jw_player_helper.rb
@@ -47,7 +47,6 @@ module PSP
       #if DEFAULT_SKIN defined then use that, otherwise look in player_options[:skin]. Don't specify if neither are set
       skin = player_options[:skin].blank? ? (PSP::JwPlayerHelper.const_defined?('DEFAULT_SKIN') ? DEFAULT_SKIN : '') : player_options[:skin]
       player_options[:skin] = "/swf/skins/#{skin}.swf" unless skin.blank?
-      
       js = "var swf_obj = new SWFObject('/swf/player.swf','#{flash_options[:player_id]}','#{flash_options[:width]}','#{flash_options[:height]}','9');" <<
            options_for_swfobject(container, flash_options, player_options)
 
